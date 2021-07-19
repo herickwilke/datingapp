f196f2964239ec37e3a27b387e651ed70b48e5cd
Hérick <herickcwilke@gmail.com>
2021-07-19 10:32:27 -0300

    Changed the code from synchronous to assynchronous execution

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using api.Data;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -18,14 +20,14 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers() {
-            return _context.Users.ToList();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+            return await _context.Users.ToListAsync();
         }
 
         // api/users/3
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id) {
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUser(int id) {
+            return await _context.Users.FindAsync(id);
         }
     }
 }
